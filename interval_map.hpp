c0e1bf60ed30fcd2fd295de71faf959d15e8131d
Anton Vasin <mail@vasin.space>
2025-08-01 22:16:02 +0200

    Gemini 2.5 pro solution

--- interval_map.hpp
+++ interval_map.hpp
@@ -21,170 +21,61 @@ public:
 	template<typename V_forward>
 	void assign(const K& keyBegin, const K& keyEnd, V_forward&& val) requires (std::is_same<std::remove_cvref_t<V_forward>, V>::value)
 	{
-		//
-		// if sequence is empty
 		if (!(keyBegin < keyEnd))
 		{
 			return;
 		}
 
-		typename std::map<K, V>::iterator it = m_map.end();
+		// --- The Single O(log N) Operation ---
+		auto it_begin = m_map.lower_bound(keyBegin);
 
-		if (val == m_valBegin)
+		// --- Determine boundary values before modification ---
+		auto it_before_begin = it_begin;
+		if (it_before_begin != m_map.begin())
 		{
-			if (m_map.empty())
-			{
-				return;
-			}
-
-			it = m_map.lower_bound(keyBegin);
-			if (it == m_map.begin())
-			{
-				typename std::map<K, V>::node_type node;
-
-				while (it != m_map.end() && (it->first < keyEnd || it->second == m_valBegin))
-				{
-					if (keyEnd < it->first && node)
-					{
-						node.key() = keyEnd;
-						m_map.insert(it, std::move(node));
-						return;
-					}
-
-					auto toExtract = it++;
-					node = m_map.extract(toExtract);
-				}
-
-				if (node && !(node.mapped() == m_valBegin))
-				{
-					node.key() = keyEnd;
-					m_map.insert(it, std::move(node));
-				}
-
-				return;
-			}
-
-			if (it == m_map.end())
-			{
-				auto prevElement = --m_map.end();
-				if (!(prevElement->second == val))
-				{
-					m_map.emplace_hint(m_map.end(), keyBegin, std::forward<V_forward>(val));
-					m_map.emplace_hint(m_map.end(), keyEnd, prevElement->second);
-				}
-
-				return;
-			}
+			--it_before_begin;
 		}
 		else
 		{
-			//
-			// if map is empty or insertion right in front of the map
-			// i.e. keyEnd <= map.begin()->frist
-			if (m_map.empty() || !(m_map.begin()->first < keyEnd))
-			{
-				if (m_map.empty() || keyEnd < m_map.begin()->first)
-				{
-					m_map.emplace_hint(m_map.begin(), keyEnd, m_valBegin);
-				}
-
-				if (!m_map.empty() && m_map.begin()->second == val)
-				{
-					auto node = m_map.extract(m_map.begin());
-					node.key() = keyBegin;
-					m_map.insert(m_map.begin(), std::move(node));
-				}
-				else
-				{
-					m_map.emplace_hint(m_map.begin(), keyBegin, std::forward<V_forward>(val));
-				}
-				return;
-			}
-
-			auto lastElement = m_map.end();
-			// if insertion after non-empty map value
-			if (!m_map.empty() && (--lastElement)->first < keyBegin)
-			{
-				if (!(lastElement->second == val))
-				{
-					m_map.emplace_hint(m_map.end(), keyBegin, std::forward<V_forward>(val));
-					m_map.emplace_hint(m_map.end(), keyEnd, lastElement->second);
-				}
-				return;
-			}
-
-			it = m_map.lower_bound(keyBegin);
-
+			it_before_begin = m_map.end(); // Use end() as a sentinel for m_valBegin
 		}
-		typename std::map<K, V>::node_type node;
-		typename std::map<K, V>::iterator prevElement = m_map.end();
-		typename std::map<K, V>::iterator inserted = m_map.end();
+		const V& value_before = (it_before_begin != m_map.end()) ? it_before_begin->second : m_valBegin;
 
-		if (it != m_map.begin())
+		auto it_end = it_begin;
+		while (it_end != m_map.end() && it_end->first < keyEnd)
 		{
-			prevElement = it;
-			--prevElement;
+			++it_end;
 		}
 
-		//
-		// if value overwrite required
-		if (!(it->first < keyBegin || keyBegin < it->first))
+		V value_at_end = m_valBegin; // Default to m_valBegin
+		if (it_end != m_map.begin())
 		{
-			const auto curElement = it++;
-
-			node = m_map.extract(curElement);
-			if (prevElement == m_map.end() || !(prevElement->second == val))
-			{
-				inserted = m_map.emplace_hint(it, keyBegin, std::forward<V_forward>(val));
-			}
+			value_at_end = std::prev(it_end)->second;
 		}
-		//
-		// if key a overwrite required
-		else if (it->second == val && !(keyEnd < it->first))
+		if (it_end != m_map.end() && !(it_end->first < keyEnd || keyEnd < it_end->first))
 		{
-			auto curElement = it++;
-			node = m_map.extract(curElement);
-			node.key() = keyBegin;
-			inserted = m_map.insert(it, std::move(node));
-			node = typename std::map<K, V>::node_type{};
-			prevElement = m_map.end();
-		}
-		//
-		// new insertion?
-		else if (prevElement == m_map.end() || !(prevElement->second == val))
-		{
-			inserted = m_map.emplace_hint(it, keyBegin, std::forward<V_forward>(val));
+			value_at_end = it_end->second;
 		}
 
-		//
-		// common case
-		while (it != m_map.end() && it->first < keyEnd)
+
+		// --- Perform modifications ---
+
+		auto erase_until = it_end;
+		if (it_end != m_map.end() && !(it_end->first < keyEnd || keyEnd < it_end->first) && it_end->second == val)
 		{
-			auto toExtract = it++;
-			node = m_map.extract(toExtract);
+			erase_until = std::next(it_end);
 		}
 
-		if (it == m_map.end() || keyEnd < it->first)
+		auto it = m_map.erase(it_begin, erase_until);
+
+		if (!(val == value_before))
 		{
-			if (node)
-			{
-				if ((inserted != m_map.end() && !(node.mapped() == inserted->second))
-					||
-					(inserted == m_map.end() && prevElement != m_map.end() && !(node.mapped() == prevElement->second)))
-				{
-					node.key() = keyEnd;
-					m_map.insert(it, std::move(node));
-				}
-			}
-			else if (it != m_map.end() && inserted != m_map.end() && prevElement != m_map.end())
-			{
-				m_map.emplace_hint(it, keyEnd, prevElement->second);
-			}
+			it = m_map.emplace_hint(it, keyBegin, val);
 		}
-		else if ((inserted != m_map.end() && it->second == inserted->second) ||
-			(inserted == m_map.end() && prevElement != m_map.end() && it->second == prevElement->second))
+
+		if (!(val == value_at_end))
 		{
-			m_map.erase(it);
+			m_map.emplace_hint(it, keyEnd, std::move(value_at_end));
 		}
 	}
 
